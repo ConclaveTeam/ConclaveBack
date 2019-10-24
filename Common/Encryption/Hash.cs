30070fc91d6d88da00ce0f1af794a736142dd7b8
Igor Maxsimov <maxsimovigorfullstack@yandex.ru>
2019-10-24 23:47:08 +0300

    Added common project and encryption classes.

---
+++ Common/Encryption/Hash.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Text;
+
+namespace Common.Encryption
+{
+    public class Hash
+    {
+        public static string Create(string value, string salt)
+        {
+            var valueBytes = KeyDerivation.Pbkdf2(
+                value,
+                Encoding.UTF8.GetBytes(salt),
+                KeyDerivationPrf.HMACSHA512,
+                10000,
+                256 / 8);
+
+            return Convert.ToBase64String(valueBytes);
+        }
+
+        public static bool Validate(string value, string salt, string hash)
+            => Create(value, salt) == hash;
+    }
+}
